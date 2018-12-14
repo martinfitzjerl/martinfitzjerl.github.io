8759291b5ee403c81f8061094a7903d7f1017dba
Samuel Baker <Samuel.Baker9@outlook.com>
2018-12-14 17:26:00 -0500

    update index.html and app.js

--- nodeApp/js/app.js
+++ nodeApp/js/app.js
@@ -1,10 +1,32 @@
 "use strict";
+
+class Namer{
+
+    constructor(name){
+        this.name = name;
+    }
+
+    sayName() {
+        alert(this.name);
+    }
+
+    
+
+    
+}
+
+let user = new Namer("John");
+
+
+
 var time = 0;
 
-console.log("Hello from nodeApp");
 
+console.log("Hello from nodeApp");
+    
+    
 setInterval(function() {
     time = time + 3;
-        console.log(time + "seconds have passed");
+    console.log(time + "seconds have passed");
     
 }, 3000);
