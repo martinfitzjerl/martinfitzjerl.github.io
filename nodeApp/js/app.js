e7678d4114d90c4fe7ede90bd1675205e61a9dee
Samuel Baker <Samuel.Baker9@outlook.com>
2018-12-14 17:03:30 -0500

    update app.js

--- nodeApp/js/app.js
+++ nodeApp/js/app.js
@@ -1,2 +1,10 @@
 "use strict";
+var time = 0;
+
 console.log("Hello from nodeApp");
+
+setInterval(function() {
+    time = time + 3;
+        console.log(time + "seconds have passed");
+    
+}, 3000);
