0f71c99937864727765829846721063b9020b046
Samuel Baker <Samuel.Baker9@outlook.com>
2018-12-14 17:43:11 -0500

    updated app.js

--- nodeApp/js/app.js
+++ nodeApp/js/app.js
@@ -27,7 +27,7 @@ console.log("Hello from nodeApp");
     
 setInterval(function() {
     time = time + 3;
-    console.log(time + "seconds have passed");
+    document.write(time + "seconds have passed");
     
 }, 3000);
 
